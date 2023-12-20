db8c2f6e6282cdbc7cb9cef7478bd0c36a77b5bf
BowmanChow <bowmanchow@gmail.com>
2023-12-21 03:50:58 +0800

    init commit

---
+++ prune_yolov5.py
@@ -0,0 +1,189 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+YOLO-specific modules
+
+Usage:
+    $ python models/yolo.py --cfg yolov5s.yaml
+"""
+
+import argparse
+import contextlib
+import os
+import platform
+import sys
+from copy import deepcopy
+from pathlib import Path
+import time
+from train import RANK
+from train import *
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+if platform.system() != 'Windows':
+    ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from thop import profile, clever_format
+from models.common import *
+from models.experimental import *
+from utils.autoanchor import check_anchor_order
+from utils.general import LOGGER, check_version, check_yaml, make_divisible, print_args
+from utils.plots import feature_visualization
+from utils.torch_utils import (ModelEMA,fuse_conv_and_bn, de_parallel, initialize_weights, model_info, profile, scale_img, select_device,
+                               time_sync)
+
+try:
+    import thop  # for FLOPs computation
+except ImportError:
+    thop = None
+
+from nni.compression.pruning import (
+    L1NormPruner,
+    L2NormPruner,
+    FPGMPruner,
+    AGPPruner
+)
+from nni.compression.speedup import ModelSpeedup
+import torch
+from torchsummary import summary
+from models.yolo import *
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='yolov5m_voc.yaml', help='model.yaml')
+    parser.add_argument('--batch-size', type=int, default=1, help='total batch size for all GPUs')
+    parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--profile', action='store_true', help='profile model speed')
+    parser.add_argument('--line-profile', action='store_true', help='profile model speed layer by layer')
+    parser.add_argument('--test', action='store_true', help='test all yolo*.yaml')
+
+    #prune
+    parser.add_argument('--pruner', default='fpgm', type=str, help='pruner: agp|taylor|fpgm')
+    parser.add_argument('--pre_weights', type=str, default="./runs/train/exp103/weights/best.pt", help='pretrain weights for prune')
+
+    opt = parser.parse_args()
+    opt.cfg = check_yaml(opt.cfg)  # check YAML
+    print_args(vars(opt))
+    device = select_device(opt.device)
+    torch.cuda.empty_cache()
+
+    # Create model
+    im = torch.rand(opt.batch_size, 3, 640, 640).to(device)
+    # model = Model(opt.cfg).to(device) #从cfg创建model，并随机初始化权重
+    ckpt = torch.load(opt.pre_weights,map_location='cpu')  # 加载预训练权重
+    model = ckpt['model'].to(device)  # 获取模型对象并将其移动到指定设备
+    model.float()  # 将模型参数转换为全精度浮点数
+    original_model_size = os.path.getsize(opt.pre_weights) / (1024 * 1024)  # 将字节转换为MB
+    print(f"剪枝之前模型的存储占用大小: {original_model_size:.2f} MB")
+
+    os.system(f"python val.py --weights {opt.pre_weights}")
+    # 读取输出的值
+    with open('output.txt', 'r') as f:
+        lines = f.readlines()
+        output1 = eval(lines[0])
+        output2 = eval(lines[1])
+    # 删除文本文件
+    os.remove('output.txt')
+    # 从output中获取map50等值
+    mp, mr, map50, map = output1
+    t1, t2, t3 = output2
+    total_time = t1+t2+t3
+    print(f"mp: {mp}, mr: {mr}, map50: {map50}, map: {map}")
+    print(f"pre-process: {t1}ms, inference: {t2}ms, NMS: {t3}ms, total_time: {total_time}ms")
+
+    # # Options
+    # if opt.line_profile:  # profile layer by layer
+    #     model(im, profile=True)
+    #     print("kkkkkkkk")
+
+    # elif opt.profile:  # profile forward-backward
+    #     results = profile(input=im, ops=[model], n=3)
+    #     print("lllllllll")
+
+
+    # elif opt.test:  # test all models
+    #     for cfg in Path(ROOT / 'models').rglob('yolo*.yaml'):
+    #         try:
+    #             _ = Model(cfg)
+    #         except Exception as e:
+    #             print(f'Error in {cfg}: {e}')
+
+    # else:  # report fused model summary
+    #     # print("hhhhhhhhh")
+    #     print(model)
+    #     # original_conv1_weights = model.model[0].conv.weight.data.clone()
+    #     model.fuse()
+    #     print(model)
+    #     # conv1_weights = model.model[0].conv.weight.data.clone()
+    #     # print("Original Conv1 Weights:")
+    #     # print(original_conv1_weights)
+
+    #     # print("\nConv1 Weights:")
+    #     # print(conv1_weights)
+    print('model paramater number: ', sum([param.numel() for param in model.parameters()]))
+    
+
+    #剪枝配置
+    config_list = [{
+                    # 'op_types': ['Conv2d'],
+                    'op_types': ['Conv2d','BatchNorm2d'],
+                    'sparse_ratio': 0.15, 
+                    # 'op_names': [],
+                    'exclude_op_names': ['model.24.m.0','model.24.m.1','model.24.m.2']
+                    }]
+    dummy_input = torch.rand(1, 3, 640, 640).to(device)
+
+    if opt.pruner == 'l1':
+        pruner = L1NormPruner(model, config_list)
+        print('Prune type：L1NormPruner')
+    elif opt.pruner == 'l2':
+        pruner = L2NormPruner(model, config_list)
+        print('Prune type：L2NormPruner')
+    # elif opt.pruner == 'agp':
+    #     config_list = [{'sparsity': args.sparsity, 'op_types': ['Conv2d']}]
+    #     pruner = AGPPruner(
+    #         model,
+    #         config_list,
+    #         optimizer = opt.optimizer,
+    #         trainer = train,
+    #         criterion = ,
+    #         num_iterations=1,
+    #         epochs_per_iteration=1,
+    #         pruning_algorithm='fpgm',
+    #     )
+    # elif opt.pruner == 'taylor':
+    #     config_list = [{'sparsity': args.sparsity, 'op_types': ['Conv2d']}]
+    #     pruner = TaylorFOWeightFilterPruner(
+    #         model,
+    #         config_list,
+    #         optimizer,
+    #         trainer,
+    #         criterion,
+    #         sparsifying_training_batches=1,
+    #     )
+    elif opt.pruner == 'fpgm':
+        pruner = FPGMPruner(model, config_list)
+        print('Prune type：FPGMPruner')
+
+    _, masks = pruner.compress()
+    pruner.unwrap_model()
+
+    pruned_model = ModelSpeedup(model, dummy_input, masks).speedup_model()
+    print(model)
+    
+    # 保存剪枝后的模型
+    ckpt = torch.load("yolov5m.pt")
+    ckpt["model"] = deepcopy(de_parallel(model))
+    ckpt["date"] = None
+    torch.save(ckpt,f'pruned_{opt.cfg.split("/")[-1].split(".")[0]}_{opt.pruner}s_{config_list[0]["sparse_ratio"]}.pt')
+    print('Pruned model paramater number: ', sum([param.numel() for param in model.parameters()]))
+    pruned_model_size = os.path.getsize(f'pruned_{opt.cfg.split("/")[-1].split(".")[0]}_{opt.pruner}s_{config_list[0]["sparse_ratio"]}.pt') / (1024 * 1024)  # 将字节转换为MB
+    print(f"剪枝之后模型的存储占用大小: {pruned_model_size:.2f} MB")
+
+
+
+    
+
